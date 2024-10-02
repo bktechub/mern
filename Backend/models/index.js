f2734cb6867b00cfb4e229ecf15e10b01f713b48
MUNEZERO BARAKA FABREGAS <130171250+fabregas250@users.noreply.github.com>
2024-10-02 18:35:52 +0200

    trial

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
-const uri = "mongodb://localhost:27017/netcompassion";
 
-
-
+// Use the correct variable for the connection string
+const url = "mongodb+srv://giranezajeandedieu2:ybP5tuy4TMg4Xa7f@cluster0.c7aff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 function main() {
-    mongoose.connect(uri).then(() => {
-        console.log("Succesfull")
-    
-    }).catch((err) => {
-        console.log("Error: ", err)
-    })
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log("Successful MongoDB connection");
+        })
+        .catch((err) => {
+            console.log("Error: ", err);
+        });
 }
 
 module.exports = { main };
